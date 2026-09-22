51f325bef94bb43c09d3d16217afedcd42106424
Jeff McBride <jeff@jeffmcbride.net>
2026-09-22 15:53:42 -0700

    Generate key with effectively infinite expiration

--- generate_rauc_key.sh
+++ generate_rauc_key.sh
@@ -5,4 +5,4 @@ if $# < 1; then
     exit 1
 fi
 
-openssl req -x509 -newkey rsa:4096 -keyout ${1}/key.pem -out ${1}/cert.pem -nodes 
+openssl req -x509 -newkey rsa:4096 -keyout ${1}/key.pem -out ${1}/cert.pem -days 36500 -nodes
